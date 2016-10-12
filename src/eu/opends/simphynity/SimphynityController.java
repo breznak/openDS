f0a97dbc1e4abe55a884d23c2199b9b2b1f438f6
Marek Otahal <markotahal@gmail.com>
2016-10-12 12:59:38 +0200

    dummy SimphynityController for missing class
    Fixes #54

---
+++ src/eu/opends/simphynity/SimphynityController.java
@@ -0,0 +1,21 @@
+package eu.opends.simphynity;
+
+import eu.opends.car.SteeringCar;
+import eu.opends.main.Simulator;
+
+/**
+ * Dummy class to fix error with missing import of this class; 
+ * Controller for MotionSeat HW, not released in ODS 4.0 sources! //FIXME
+ * @author mmm
+ */
+public class SimphynityController {
+
+    public SimphynityController(Simulator sim, SteeringCar aThis, String ip, int port) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+                            
+                        }
